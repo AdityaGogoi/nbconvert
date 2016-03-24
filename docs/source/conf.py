e7ff0b861c6b706e52a99fe2bb49f687b731340b
Carol Willing <carolcode@willingconsulting.com>
2016-03-24 16:51:46 -0700

    Updateconfig for nice docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -129,9 +129,7 @@ todo_include_todos = False
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    from jupyter_sphinx_theme import *
 
 # otherwise, readthedocs.org uses their default theme, so no need to specify it
 
